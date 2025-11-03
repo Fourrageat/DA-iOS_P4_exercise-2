6e23140764429429f23f30f93373f1c5797db79a
Fourrageat <baptiste.fourrageat@icloud.com>
2025-11-03 14:28:28 +0100

    wip

--- UserList/Sources/UserListView.swift
+++ UserList/Sources/UserListView.swift
@@ -54,18 +54,7 @@ struct UserListView: View {
                         ForEach(viewModel.users) { user in
                             NavigationLink(destination: UserDetailView(user: user)) {
                                 VStack {
-                                    AsyncImage(url: URL(string: user.picture.medium)) { image in
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: 150, height: 150)
-                                            .clipShape(Circle())
-                                    } placeholder: {
-                                        ProgressView()
-                                            .frame(width: 150, height: 150)
-                                            .clipShape(Circle())
-                                    }
-                                    
+                                    ImageView(pictureUrl: user.picture.medium, size: 150)
                                     Text("\(user.name.first) \(user.name.last)")
                                         .font(.headline)
                                         .multilineTextAlignment(.center)
